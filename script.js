aefe299897b94fdabb162b42a2a2beca433d7360
Blake Davis <Blakedav2088@gmail.com>
2021-09-04 02:44:27 -0400

    added functionality to add a book object to the library array

--- script.js
+++ script.js
@@ -15,3 +15,19 @@ Book.prototype.info = function() {
 const book = new Book("Clean Code", "Uncle Rob", 300, false);
 
 console.log(book.info());
+
+function addBookToLibrary(book){
+    myLibrary.push(book);
+}
+
+addBookToLibrary(book);
+
+console.log(myLibrary);
+
+function addBooksToPage(library){
+    library.forEach((book) => {
+        
+
+    });
+
+}
