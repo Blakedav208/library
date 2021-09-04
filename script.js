33ca1f9d90bb8444b5e92b6538df5a0b7b3192fc
Blake Davis <Blakedav2088@gmail.com>
2021-09-04 00:12:37 -0400

    basic functionality to create a book object with an object constructor

---
+++ script.js
@@ -0,0 +1,17 @@
+let myLibrary = [];
+
+function Book(title, author, numPages, hasRead){
+    this.title = title;
+    this.author = author;
+    this.numPages = numPages;
+    this.hasRead = hasRead;
+
+}//end of Book object constructor
+
+Book.prototype.info = function() {
+    return this.title + " by " + this.author + ", " + this.numPages + ", " + this.hasRead;
+}
+
+const book = new Book("Clean Code", "Uncle Rob", 300, false);
+
+console.log(book.info());
