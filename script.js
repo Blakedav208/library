018b1138d08445daaeab10491f2178a996715623
Blake Davis <Blakedav2088@gmail.com>
2021-09-09 01:40:56 -0400

    added functionality to add a book to the library through the webpage, only console logs so far

--- script.js
+++ script.js
@@ -1,33 +1,58 @@
 let myLibrary = [];
 
-function Book(title, author, numPages, hasRead){
-    this.title = title;
-    this.author = author;
-    this.numPages = numPages;
-    this.hasRead = hasRead;
+function Book(title, author, numPages, hasRead) {
+  this.title = title;
+  this.author = author;
+  this.numPages = numPages;
+  this.hasRead = hasRead;
+} //end of Book object constructor
 
-}//end of Book object constructor
-
-Book.prototype.info = function() {
-    return this.title + " by " + this.author + ", " + this.numPages + ", " + this.hasRead;
-}
+Book.prototype.info = function () {
+  return (
+    this.title +
+    " by " +
+    this.author +
+    ", " +
+    this.numPages +
+    ", " +
+    this.hasRead
+  );
+};
 
 const book = new Book("Clean Code", "Uncle Rob", 300, false);
 
-console.log(book.info());
+const book2 = new Book("How to Code", "Blake Davis", 250, false);
 
-function addBookToLibrary(book){
-    myLibrary.push(book);
+const book3 = new Book("Learn to Code", "Lauren Davis", 400, true);
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
 }
 
-addBookToLibrary(book);
 
-console.log(myLibrary);
 
-function addBooksToPage(library){
-    library.forEach((book) => {
-        
 
-    });
 
-}
+function addBooksToPage(library) {
+  library.forEach((book) => {
+    console.log(book);
+  });
+} //end of add books to page
+
+//========JS ===========
+
+const library = document.querySelector(".library");
+const ui = document.querySelector(".ui");
+const addBookBtn = document.getElementById("addBook");
+
+const authorField = document.getElementById("title");
+const titleField = document.getElementById("author");
+const pagesField = document.getElementById("numPages");
+const hasReadField = document.getElementById("hasRead");
+
+
+addBookBtn.addEventListener('click', () => {
+    let newBook = new Book(authorField.value, titleField.value, parseInt(pagesField.value), hasReadField.value);
+    addBookToLibrary(newBook);
+    console.log(myLibrary);
+});
